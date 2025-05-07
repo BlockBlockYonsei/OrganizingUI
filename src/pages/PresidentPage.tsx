701d5505c1f8870934bf5039a963e3a982d7d1e6
guboc11 <guboc11@gmail.com>
2025-05-08 01:02:15 +0900

    Add ExecutiveMember

--- src/pages/PresidentPage.tsx
+++ src/pages/PresidentPage.tsx
@@ -1,9 +1,12 @@
+import ExecutiveMember from "@/pages-component/ExecutiveMember";
 import NextClubClass from "@/pages-component/NextClubClass";
 
 export default function PresidentPage() {
   return (
     <section className="p-12">
       <h2 className="text-3xl font-bold mb-10 text-white">President Page</h2>
+      <ExecutiveMember></ExecutiveMember>
+
       <NextClubClass></NextClubClass>
     </section>
   );

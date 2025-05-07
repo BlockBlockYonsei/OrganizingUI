69e0cff14a5decf1dd31bce59d82bf0e3efee93f
guboc11 <guboc11@gmail.com>
2025-05-08 00:46:07 +0900

    pull origin/dev

--- src/pages/PresidentPage.tsx
+++ src/pages/PresidentPage.tsx
@@ -1,27 +1,6 @@
 import NextClubClass from "@/pages-component/NextClubClass";
-import { useState } from "react";
-import { Card } from "@/components/ui/card";
-import { useGetExecutiveMemberCap } from "@/hooks/executive-member";
-import { useCurrentAccount } from "@mysten/dapp-kit";
-import { useEffect } from "react";
 
 export default function PresidentPage() {
-  const [roles, setRoles] = useState({
-    president: false,
-    vpresident: false,
-    treasurer: false,
-  });
-
-  const allDone = Object.values(roles).every(Boolean);
-
-  const toggleRole = (key: keyof typeof roles) => {
-    setRoles((prev) => ({ ...prev, [key]: !prev[key] }));
-  };
-
-  const handleArrowClick = () => {
-    console.log("화살표 클릭됨");
-  };
-
   return (
     <section className="p-12">
       <h2 className="text-3xl font-bold mb-10 text-white">President Page</h2>

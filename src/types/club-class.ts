8c52970491ab6afe0c3b0819c51f1d59b87f2285
guboc11 <guboc11@gmail.com>
2025-05-08 01:36:55 +0900

    Add CurrentClass, PastClass Type

--- src/types/club-class.ts
+++ src/types/club-class.ts
@@ -3,3 +3,17 @@ export interface CreateNewClassEvent {
   class_id: string;
   class: number;
 }
+
+export interface CurrentClass {
+  id: string;
+  blockblock_ys: string;
+  class: number;
+  is_open_for_new_members: boolean;
+}
+
+export interface PastClass {
+  id: string;
+  blockblock_ys: string;
+  class: number;
+  next_class_id: string;
+}

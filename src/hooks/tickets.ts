9b4e2ed2a581f4f40ff14fb2e717dc31351f0bb3
guboc11 <guboc11@gmail.com>
2025-05-17 00:22:34 +0900

    Add ExecutiveMemberTicket type

---
+++ src/hooks/tickets.ts
@@ -0,0 +1,71 @@
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
+import { useEffect, useState } from "react";
+import { PACKAGE_ID } from "@/Constant";
+import { useGetCurrentClass } from "./club";
+import { BlockblockMember, ExecutiveMember } from "@/types/members";
+
+export const useGetExecutiveMemberTicket = ({ owner }: { owner: string }) => {
+  const [caps, setCaps] = useState<ExecutiveMember[]>([]);
+  const [isPending, setIsPending] = useState<boolean>(true);
+  const [error, setError] = useState(null);
+
+  const CAP_TYPE = `${PACKAGE_ID}::executive_member::ExecutiveMemberTicket`;
+
+  const { currentClass } = useGetCurrentClass();
+
+  const client = new SuiClient({ url: getFullnodeUrl("testnet") });
+  useEffect(() => {
+    if (!owner) return;
+    if (!currentClass) return;
+
+    client
+      .getOwnedObjects({
+        owner,
+        filter: { StructType: CAP_TYPE },
+        options: {
+          showType: true,
+          showContent: true,
+        },
+      })
+      .then((data) => {
+        const currentClassExecutiveMemberCaps = data.data.flatMap((d) => {
+          const content = d.data?.content;
+          if (
+            content &&
+            "fields" in content &&
+            "club_class" in content.fields &&
+            typeof content.fields.club_class === "string" &&
+            "id" in content.fields &&
+            typeof content.fields.id === "object" &&
+            content.fields.id !== null &&
+            "id" in content.fields.id &&
+            typeof content.fields.id.id === "string" &&
+            "member_type" in content.fields &&
+            typeof content.fields.member_type === "string"
+          ) {
+            const executiveMember: ExecutiveMember = {
+              id: content.fields.id.id,
+              club_class: Number(content.fields.club_class),
+              member_type: content.fields.member_type,
+            };
+
+            if (executiveMember.club_class === currentClass.class) {
+              return executiveMember;
+            }
+          }
+          return [];
+        });
+        console.log("FilteredData", currentClassExecutiveMemberCaps);
+        setCaps(currentClassExecutiveMemberCaps);
+        setIsPending(false);
+      })
+      .catch((e) => setError(e))
+      .finally();
+  }, [owner, currentClass]);
+
+  return {
+    caps,
+    isPending,
+    error,
+  };
+};

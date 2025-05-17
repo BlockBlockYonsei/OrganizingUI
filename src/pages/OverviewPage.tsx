e55934c3f0da92842fd579e430f661c050be9b35
guboc11 <guboc11@gmail.com>
2025-05-17 15:50:41 +0900

    Add CurrentClass info to OverviewPage

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,13 +1,36 @@
 import WalletButton from "@/components/layout/WalletButton";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useCurrentClass, useGetCurrentClass } from "@/hooks/club";
 import { useCurrentAccount } from "@mysten/dapp-kit";
 import { Link } from "react-router-dom";
 
 export default function OverviewPage() {
   const account = useCurrentAccount();
+  const { currentClass } = useGetCurrentClass();
   return (
     <div>
-      <h1>지갑 연결 페이지</h1>
-
+      {currentClass && (
+        <Card className="w-90">
+          <CardHeader>
+            <CardTitle>Current Club Class: {currentClass.class}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            {Object.entries(currentClass).map(([name, value]) => {
+              if (name === "recruitment")
+                return (
+                  <CardTitle className="truncate w-full">
+                    {name}: {JSON.stringify(value)}
+                  </CardTitle>
+                );
+              return (
+                <CardTitle className="truncate">
+                  {name}: {value}
+                </CardTitle>
+              );
+            })}
+          </CardContent>
+        </Card>
+      )}
       <br />
 
       {account ? (
@@ -24,8 +47,11 @@ export default function OverviewPage() {
           </Link>
         </div>
       ) : (
-        <div className="w-45">
-          <WalletButton />
+        <div>
+          <h1>지갑 연결 페이지</h1>
+          <div className="w-45">
+            <WalletButton />
+          </div>
         </div>
       )}
     </div>
